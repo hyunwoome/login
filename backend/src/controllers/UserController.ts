6223005e57e748e7d21a69c6fe6875fe13d421ce
hyunwoome <hyunwoome@icloud.com>
2021-09-17 19:52:29 +0900

    [BE] 회원가입 유효성 검사 작성

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,68 +1,61 @@
 import { Request, Response, NextFunction } from 'express';
 import UserInterface from '../interfaces/UserInterface';
 import bcryptPassword from '../middlewares/bcrypt';
-import AuthService from '../services/AuthService';
 import UserService from '../services/UserService';
-import validator from '../middlewares/validation';
-import errorGenerator from '../error/errorGenerator';
+import UserValidation from '../validators/UserValidation';
 
-// 1. 이름, 이메일, 패스워드 필수 체크 (DB에서 체크)
-// 2. 이름은 1글자 이상 (유효성 검사) (OK)
-// 3. 이메일 양식 확인 (유효성 검사) (OK)
-// 4. 이메일 중복 확인 (DB에서 체크)
-// 5. 2차 패스워드 동일 여부 확인 (유효성 검사) (OK)
-// 6. 패스워드 6자 이상 확인 (유효성 검사) (OK)
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, email, password, checkPassword }: UserInterface.IUser =
       req.body;
-    validator.nameChecker(name);
-    validator.emailChecker(email);
-    validator.passwordChecker(password, checkPassword);
-    const foundUser = await AuthService.findEmail({ email });
-    if (foundUser) errorGenerator({ statusCode: 409 });
-    else {
-      await UserService.createUser({
-        name,
-        email,
-        password: await bcryptPassword(password),
-        checkPassword: await bcryptPassword(checkPassword),
-      });
-      res.send('회원가입 성공');
-    }
+    UserValidation.nameChecker(name);
+    UserValidation.emailChecker(email);
+    UserValidation.passwordChecker(password, checkPassword);
+    await UserValidation.emailDuplicateChecker(email);
+    await UserService.createUser({
+      name,
+      email,
+      password: await bcryptPassword(password),
+      checkPassword: await bcryptPassword(checkPassword),
+    });
+    res.status(200).json({ message: '회원가입 성공' });
   } catch (error) {
     next(error);
   }
 };
 
-const readUser = async (req: Request, res: Response) => {
+const readUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.params;
     const user = await UserService.readUser({ userId });
     res.send(user);
   } catch (error) {
-    res.send(error);
+    next(error);
   }
 };
 
-const readUsers = async (req: Request, res: Response) => {
+const readUsers = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const users = await UserService.readUsers();
     res.send(users);
   } catch (error) {
-    res.send(error);
+    next(error);
   }
 };
 
 // TODO:
-const updateUser = async (req: Request, res: Response) => {};
+const updateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {};
 
-const deleteUser = async (req: Request, res: Response) => {
+const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { userId } = req.params;
     res.send(await UserService.deleteUser({ userId }));
   } catch (error) {
-    res.send(error);
+    next(error);
   }
 };
 
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,14 +3,12 @@ import AuthController from '../controllers/AuthController';
 import UserController from '../controllers/UserController';
 const router = Router();
 
-// User
 router.post('/api/user', UserController.createUser);
 router.get('/api/user/:userId', UserController.readUser);
 router.get('/api/users', UserController.readUsers);
 router.put('/api/user/:userId', UserController.updateUser);
 router.delete('/api/user/:userId', UserController.deleteUser);
 
-// Auth
 router.post('/api/auth/login', AuthController.logIn);
 
 export default router;
