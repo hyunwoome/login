11d34e6af45c55806fbaf1b130f4f3309647828a
hyunwoome <hyunwoome@icloud.com>
2021-10-12 16:55:57 +0900

    [BE] 로그아웃 컨트롤러 정의

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,22 +1,30 @@
-import { NextFunction, Request, Response } from 'express';
-import { AuthValidation } from '@src/validators';
+import {NextFunction, Request, Response} from 'express';
+import {AuthValidation} from '@src/validators';
 
 const logIn = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
     const user = await AuthValidation.emailFinder(email);
     await AuthValidation.comparePassword(password, user!.password);
     req.session.isAuth = true;
     req.session.email = email;
-    console.log(req.session);
-    res.status(200).json({ loginSuccess: true, email });
+    res.status(200).json({loginSuccess: true, email});
   } catch (error) {
     next(error);
   }
 };
 
 const logOut = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.sessionID);
+  try {
+    if (req.session.isAuth) {
+      req.session.destroy((err) => console.error(err));
+      res.status(200).json({loginSuccess: false});
+    } else {
+      throw new Error('세션이 없습니다');
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 const AuthController = {
