40dd7493db38af200661d1409b930732c0ff95bf
hyunwoome <hyunwoome@icloud.com>
2021-10-25 23:13:58 +0900

    [FE] API URL 변경

--- frontend/src/apis/axios.ts
+++ frontend/src/apis/axios.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-export const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:3000' : '';
-
-const axiosIns = axios.create({ baseURL, withCredentials: true});
+const axiosIns = axios.create({ withCredentials: true});
 
 export {axiosIns};
